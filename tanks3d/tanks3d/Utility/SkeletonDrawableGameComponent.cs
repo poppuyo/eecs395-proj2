6548d59e673b4763cc9c75070cdcd64bf4c42d7b
sergkr <serg.kr@u.northwestern.edu>
2011-05-07 22:49:55 -0500

    Adding in a skeleton DrawableGameComponent class. It's never actually instantiated, it's just a skeleton you can copy/paste if you want to add a new kind of DrawableGameComponent to the game.

---
+++ tanks3d/tanks3d/Utility/SkeletonDrawableGameComponent.cs
@@ -0,0 +1,35 @@
+﻿// A skeleton file for creating a new DrawableGameComponent.
+// Don't forget to do Components.Add().
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace tanks3d
+{
+    class SkeletonDrawableGameComponent : DrawableGameComponent
+    {
+        private Game1 game;
+
+        public SkeletonDrawableGameComponent(Game1 g) : base(g) 
+        {
+            game = g;
+        }
+
+        protected override void LoadContent()
+        {
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            float elapsedSeconds = (float)gameTime.ElapsedGameTime.TotalSeconds;
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+        }
+    }
+}
